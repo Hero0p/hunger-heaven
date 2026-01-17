cc06fd283cad47b54eaeaa64b1960f0dbc74c753
Hero0p <nishantchaudhary03115@gmail.com>
2026-01-18 04:31:00 +0530

    food API completed with authentication and imagekit integration

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,9 +1,21 @@
 const foodModel = require("../models/food.model");
+const storageService = require("../services/storage.service");
+const {v4 : uuid} = require("uuid");
 
 async function createFood(req , res){
-    console.log(req.body);
-    console.log(req.file);
-    res.send("item created");
+    // console.log(req.body);
+    // console.log(req.file);
+    const fileUploadResult = await storageService.uploadFile(req.file.buffer , uuid());
+    // console.log(fileUploadResult);
+    const foodItem = await foodModel.create({
+        name: req.body.name,
+        description: req.body.description,
+        video: fileUploadResult.url,
+        foodPartner: req.body.foodPartner
+    })
+
+
+    return res.status(201).json({message : "food created successfully" , food : foodItem});
 
 }
 
