2a6e72db00b5760da187dd23f3ecb4c001815110
Hero0p <nishantchaudhary03115@gmail.com>
2026-02-01 18:30:43 +0530

    local mongo to atlas

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-function connectDb(){
-    mongoose.connect('mongodb://localhost:27017/hunger-heaven')
-    .then(() => {
-        console.log("databse connected");
-    }).catch((e) => {
-        console.log("db connection error : ", e);
-    })
+function connectDb() {
+    mongoose.connect(process.env.MONGODB_URL)
+        .then(() => {
+            console.log("databse connected");
+        }).catch((e) => {
+            console.log("db connection error : ", e);
+        })
 }
 
 module.exports = connectDb;
